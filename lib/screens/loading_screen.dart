295da673e7101af02ef556e6eaad7c11c98eb076
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2022-10-16 13:24:17 +0300

    written location class

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,48 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:weather/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  Position? _position;
-
-  void _getCurrentLocation() async{
-    Position position = await _determinePosition();
-    //await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    setState(() {
-      _position= position;
-    });
-    print(position);
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
   }
 
-  Future<Position> _determinePosition() async {
-    LocationPermission permission;
-    permission = await Geolocator.checkPermission();
-    if(permission==LocationPermission.denied){
-      permission= await Geolocator.requestPermission();
-      if(permission==LocationPermission.denied){
-        return Future.error('Location Permissions are denied');
-      }
-    }
-    return await Geolocator.getCurrentPosition();
+  void getLocation() async{
+    Location location=Location();
+    await location.getCurrentLocation();
+    print(location.longitude);
+    print(location.latitude);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-            _getCurrentLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
---
+++ lib/services/location.dart
@@ -0,0 +1,29 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double? latitude;
+  double? longitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await _determinePosition();
+      latitude=position.latitude;
+      longitude=position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<Position> _determinePosition() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location Permissions are denied');
+      }
+    }
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+  }
+}
